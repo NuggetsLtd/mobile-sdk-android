4cf35e25f638d7b330a4130ba5b4ff0c3aeabe7c
Andrew Lord <andrew.lord@nuggets.life>
2024-02-14 14:06:39 +0000

    :wrench:  Add `publishing` config:wq

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,11 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+        }
+    }
 }
 
 dependencies {
