c79f8059ab60928e4d06deed0c5447f9058f6726
Andrew Lord <andrew.lord@nuggets.life>
2024-02-14 17:12:59 +0000

    Update Android SDK

--- NuggetsSDKAndroid/build.gradle.kts
+++ NuggetsSDKAndroid/build.gradle.kts
@@ -45,5 +45,5 @@ dependencies {
     // load third-party dependencies
     File("${rootDir}/NuggetsSDKAndroid/libs.txt").forEachLine { api("$it") }
     
-    api("com.github.NuggetsLtd:mobile-sdk-android-libs:v0.0.39")
+    api("com.github.NuggetsLtd:mobile-sdk-android-libs:v0.0.40")
 }
