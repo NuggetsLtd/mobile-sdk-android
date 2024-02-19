87937cf4e5034b1fcc12f5f3ad62dfb96268d165
Andrew Lord <andrew.lord@nuggets.life>
2024-02-19 09:12:43 +0000

    :bookmark:  Add changed versions following git add publish script failure

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "life.nuggets"
             artifactId = "mobilesdk"
-            version = "0.0.69"
+            version = "0.0.70"
             println("This is executed during the configuration phase.")
 
             afterEvaluate {
