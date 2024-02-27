79a50795c5762cc6cc0a2533317247e2e6641ced
Andrew Lord <andrew.lord@nuggets.life>
2024-02-27 15:39:45 +0000

    Update Android SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "life.nuggets"
             artifactId = "mobilesdkandroid"
-            version = "0.0.79"
+            version = "0.0.80"
             println("app: This is executed during the configuration phase.")
 
             afterEvaluate {
