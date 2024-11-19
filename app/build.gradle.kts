022bd4c8159e4dc494d889d3ae7e2d670520a36b
Samuel Johnson <155970973+samnuggets@users.noreply.github.com>
2024-11-19 20:13:27 +0000

    Update Android SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "life.nuggets"
             artifactId = "mobilesdkandroid"
-            version = "3.0.2"
+            version = "3.0.3"
             println("app: This is executed during the configuration phase.")
 
             afterEvaluate {
--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "life.nuggets"
             artifactId = "mobilesdk"
-            version = "3.0.2"
+            version = "3.0.3"
             println("This is executed during the configuration phase.")
 
             afterEvaluate {
