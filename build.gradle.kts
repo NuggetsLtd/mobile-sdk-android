c5ff8f1427e1f996becb43c47881fc78b8f6f08d
machine-circle-ci <machine@nuggets.life>
2024-12-19 14:19:46 +0000

    Update Android SDK

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "life.nuggets"
             artifactId = "mobilesdk"
-            version = "3.0.9"
+            version = "3.0.10"
             println("This is executed during the configuration phase.")
 
             afterEvaluate {
