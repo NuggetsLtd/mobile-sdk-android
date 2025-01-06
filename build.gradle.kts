e4bfcf21cd1e5101b8b50ecf2b70d88f6a4d4e12
Samuel Johnson <155970973+samnuggets@users.noreply.github.com>
2025-01-06 23:22:25 +0000

    Update Android SDK

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "life.nuggets"
             artifactId = "mobilesdk"
-            version = "3.0.11"
+            version = "3.0.12"
             println("This is executed during the configuration phase.")
 
             afterEvaluate {
--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     // load third-party dependencies
     File("${rootDir}/sdk/libs.txt").forEachLine { api("$it") }
     
-    api("com.github.NuggetsLtd:mobile-sdk-android-libs:v3.0.11")
+    api("com.github.NuggetsLtd:mobile-sdk-android-libs:v3.0.12")
 }
 
 publishing {
@@ -53,7 +53,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "life.nuggets"
             artifactId = "sdk"
-            version = "3.0.11"
+            version = "3.0.12"
             println("sdk: This is executed during the configuration phase.")
 
             afterEvaluate {
